959beec1c394498601c584b4704a6a9dcfa08f09
Vishnu Eskew <vishnu.eskew@gmail.com>
2024-12-11 18:37:48 -0500

    Add helper function to match bibd to names, and add bibd list created from online sagecell.

--- card_helpers.py
+++ card_helpers.py
@@ -1,6 +1,7 @@
 from PIL import ImageDraw
 import math
 
+
 def draw_hexagon(image, center, radius, fill_color, outline_color):
     """
     Draw a regular hexagon on the given image.
@@ -25,3 +26,41 @@ def draw_hexagon(image, center, radius, fill_color, outline_color):
     # Draw the hexagon on the image
     draw = ImageDraw.Draw(image)
     draw.polygon(points, fill=fill_color, outline=outline_color)
+
+def card_symbols(bibd_list, names):
+    """
+    Function that takes a bibd and peoples' names and creates a spot it deck.
+    
+    Args:
+        bibd_list: This is a list of lists, with each individual lists
+    representing a bibd largly populated by 0s with the occasional 1. Each list
+    has the same number of 1s.
+        names: This is a list with the name of everyone in Discrete in the spot
+    it deck.
+
+    Returns:
+        A list of lists, where each individual list is a card in our deck and
+    has the names of the people going on that card.
+    """
+
+    result = []
+    for i in bibd_list:
+        temp = []
+        for j in i:
+            temp.append(names[j])
+        result.append(temp)
+    return result
+    """
+    result = []
+    # loop through every list in the BIBD
+    for i in bibd_list:
+        temp1 = 0
+        temp2 = []
+        # loop through every item in each BIBD list
+        for j in i:
+            if j == 1:
+                temp2.append(names[temp1])
+            temp1 =+ 1
+        result.append(temp1)
+    return result
+    """
