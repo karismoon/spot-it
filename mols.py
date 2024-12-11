7bb4ee73ad654a2cfe3c2423017510d6c652c789
Karis Moon <kmoon@olin.edu>
2024-12-11 18:21:45 -0500

    Func to assign indices

--- mols.py
+++ mols.py
@@ -3,6 +3,10 @@ Generate a spot it deck using MOLS.
 """
 
 from pic_library import file_names
-from mols_helpers import print_mols
+from mols_helpers import (
+    print_mols,
+    index_objects
+)
 
 print_mols(file_names)
+square = index_objects(file_names)
--- mols_helpers.py
+++ mols_helpers.py
@@ -11,6 +11,7 @@ def print_mols(file_names):
     Args:
         file_names (list): Names of the symbols
     """
+    # Print square
     for index in range(9, len(file_names)):
         if index % 8 != 0:
             print(f"{file_names[index]} ", end = "")
@@ -19,6 +20,7 @@ def print_mols(file_names):
 
     print("\n")
 
+    # Print infinity points
     print("Infinity Points")
     for index in range(9):
         print(f"{infinity_points[index]}: {file_names[index]}")
@@ -31,9 +33,39 @@ def index_objects(file_names):
         file_names (list): Names of the symbols.
 
     Returns: 
-        square (dict): All of the symbols mapped to their locations in the MOLS.
+        A dict representing all of the symbols mapped to their locations in the MOLS including infinity points.
     """
     square = {}
 
+    # Assign infinity points
     for index in range(9):
-        infinity_points[index] = square[file_names(index)]
+        square[infinity_points[index]] = file_names[index]
+
+    list_counter = 9
+
+    # Assign rest of symbols to square
+    for row in range(8):
+        for column in range(8):
+            square[(row, column)] = file_names[list_counter]
+            list_counter += 1
+
+    return square
+
+def next_square(dimension, square, current):
+    """
+    Find the next square along the diagonal for a specific dimension of an MOLS.
+    
+    Args:
+        dimension (string or int): Dimension that the diagonal is traveling across.
+        square (dict): Representation of the MOLS being traveled on.
+        current (string): Name of symbol that is being traveled away from. 
+
+    Returns: 
+        A string representing the next symbol in the diagonal.
+    """
+    if dimension == "horizontal":
+        next_location = (square[current](0, square[current](1) + 1)
+        
+    if square[current](1) < dimension:
+        # Overflow, wrap to other side of square
+        
